d05937833bd698f90f1639172ce697bcfb8b032f
huzaifanizam <nizamhuzaifa@gmail.com>
2023-10-27 14:50:26 +0500

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//  new Game Functions 
 let div = document.getElementById('boxNewGame');
 let display = 0;
 
@@ -7,6 +8,48 @@ function newGmae() {
         display = 0;
     } else {
         div.style.display = 'none';
+        display = 1;
+    }
+};
+//  Find Servers Functions 
+
+let Serversdiv = document.getElementById('boxFindServers');
+// let display = 0;
+
+function findServers() {
+    if (display == 1) {
+        Serversdiv.style.display = 'block';
+        display = 0;
+    } else {
+        Serversdiv.style.display = 'none';
+        display = 1;
+    }
+}
+
+//  option Functions 
+
+let optiondiv = document.getElementById('boxOption');
+
+function option() {
+    if (display == 1) {
+        optiondiv.style.display = 'block';
+        display = 0;
+    } else {
+        optiondiv.style.display = 'none';
+        display = 1;
+    }
+}
+
+//  quit Functions 
+
+let quitdiv = document.getElementById('boxQuit');
+
+function quit() {
+    if (display == 1) {
+        quitdiv.style.display = 'block';
+        display = 0;
+    } else {
+        quitdiv.style.display = 'none';
         display = 1;
     }
 }
